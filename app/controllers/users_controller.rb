deb76c4ad25d2de141bb58fbc7a4c7e02f86784c
albane tonnellier <albanetonnellier@gmail.com>
2020-11-24 14:45:20 +0000

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,19 @@ class UsersController < ApplicationController
 
   def index
     # filter to pros only
+    @users = User.where(pro: true)
   end
 
   def show
     # show the profile page
+    @user = User.find(params[:id])
+    if @user.pro
+      # show pro profile
+      render :pro
+    else
+      # show user profile
+      render :show
+    end
   end
 end
+
--- db/migrate/20201124111603_remove_genre_to_project.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGenreToProject < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :services, :genre
-  end
-end
