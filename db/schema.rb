5d24b82a1a53581e26d848d791712003bce36b7d
Diego Philips <diego.r.philips@gmail.com>
2020-11-24 15:52:34 +0000

    added has_many

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_145837) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
