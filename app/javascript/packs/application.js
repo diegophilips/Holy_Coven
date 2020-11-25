a796cc133873ae276550c9948ab5f889fd884247
albane tonnellier <albanetonnellier@gmail.com>
2020-11-25 21:38:44 +0000

    dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { dropdownhome } from '../pages/dropdownhome';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
