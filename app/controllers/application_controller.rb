2baa12e6913efc0df3de893da835125997cb4a78
Kotomyra Uchida <kotomyrau@gmail.com>
2020-11-30 13:04:02 +0000

    added album cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :artist_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :artist_name, :bio, :spotify_artist_link])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:artist_name, :bio, :spotify_artist_link, :facebook_link, :instagram_link])
+
   end
 end
