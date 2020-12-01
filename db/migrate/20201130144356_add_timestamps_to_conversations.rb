ab954165232f6741a1bcb9dac78bf34896e21315
Diego Philips <diego.r.philips@gmail.com>
2020-12-01 09:38:43 +0000

    fixed stuff

--- db/migrate/20201130144356_add_timestamps_to_conversations.rb
+++ db/migrate/20201130144356_add_timestamps_to_conversations.rb
@@ -1,7 +1,6 @@
 class AddTimestampsToConversations < ActiveRecord::Migration[6.0]
   def change
-    add_column :conversations, :created_at, :datetime, null: false
-    add_column :conversations, :updated_at, :datetime, null: false
     remove_column :conversations, :timestamps, :datetime
+    add_timestamps :conversations, default: Time.zone.now
   end
 end
