ecab13c29fa158030eb6cc06bfb55b8507e0c45d
Diego Philips <diego.r.philips@gmail.com>
2020-12-02 11:57:55 +0000

    images are resized for fluidity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,15 @@ module ApplicationHelper
     if service.type_of_service.strip.downcase == "marketing"
       "Marketing.jpg"
     elsif service.type_of_service.strip.downcase == "production"
-      "production.png"
+      "production.jpg"
     elsif service.type_of_service.strip.downcase == "creative coaching"
-      "creative.png"
+      "creative.jpg"
     elsif service.type_of_service.strip.downcase == "social media management"
-      "social.png"
+      "social.jpg"
     elsif service.type_of_service.strip.downcase == "pr and playlisting"
-      "pr.png"
+      "pr.jpg"
     elsif service.type_of_service.strip.downcase == "legal & administrative advising"
-      "legal.png"
+      "legal.jpg"
     end
   end
 end
